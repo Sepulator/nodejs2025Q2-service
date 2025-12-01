a5ff88591645d7724fea71594522fe2406a8666e
Yuri Skrypal <yjura@protonmail.com>
2025-12-01 10:38:08 +0200

    feat: correct database relations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { DbModule } from './db/db.module';
-import { ArtistModule } from './artist/artist.module';
-import { AlbumModule } from './album/album.module';
-import { TrackModule } from './track/track.module';
-import { FavsModule } from './favs/favs.module';
+
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, DbModule, ArtistModule, AlbumModule, TrackModule, FavsModule],
+  imports: [UserModule, PrismaModule],
   controllers: [],
   providers: [],
 })
