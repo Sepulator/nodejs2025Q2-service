22dbf2e65e934337f25e05b8de07b2459d1b5742
Yuri Skrypal <yjura@protonmail.com>
2025-12-09 12:39:07 +0200

    feat: add jwt tokens for login endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,13 +3,20 @@ import { CreateAuthDto } from './dto/create-auth.dto';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { User } from 'src/user/entities/user.entity';
-import { convertUserDate } from 'src/utils/convert-date';
+import { JwtService, JwtSignOptions } from '@nestjs/jwt';
+import { Tokens } from './types/tokens.interface';
+import { ConfigService } from '@nestjs/config';
+import { StringValue } from 'ms';
 
 const saltOrRounds = 10;
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
   async signup(createAuthDto: CreateAuthDto): Promise<Omit<User, 'password'>> {
     const { login, password } = createAuthDto;
@@ -20,7 +27,10 @@ export class AuthService {
       throw new ConflictException('User with this login already exists');
     }
 
-    const hashedPassword = await bcrypt.hash(password, parseInt(process.env.CRYPT_SALT, 10) || saltOrRounds);
+    const hashedPassword = await bcrypt.hash(
+      password,
+      parseInt(this.configService.get<string>('CRYPT_SALT'), 10) || saltOrRounds,
+    );
 
     const newUser = await this.userService.create({
       login,
@@ -30,7 +40,7 @@ export class AuthService {
     return newUser;
   }
 
-  async login(createAuthDto: CreateAuthDto): Promise<Omit<User, 'password'>> {
+  async login(createAuthDto: CreateAuthDto): Promise<Tokens> {
     const { login, password } = createAuthDto;
     const user = await this.userService.findUserByLogin(login);
 
@@ -44,6 +54,30 @@ export class AuthService {
       throw new ForbiddenException('Wrong password');
     }
 
-    return convertUserDate(user);
+    return this.getTokens(user.id, user.login);
+  }
+
+  async getTokens(userId: string, login: string): Promise<Tokens> {
+    const payload = { sub: userId, login };
+
+    const jwtSignOptionsAccess: JwtSignOptions = {
+      secret: this.configService.get<string>('JWT_SECRET_KEY'),
+      expiresIn: this.configService.get<string>('TOKEN_EXPIRE_TIME') as StringValue,
+    };
+
+    const jwtSignOptionsRefresh: JwtSignOptions = {
+      secret: this.configService.get<string>('JWT_SECRET_REFRESH_KEY'),
+      expiresIn: this.configService.get<string>('TOKEN_REFRESH_EXPIRE_TIME') as StringValue,
+    };
+
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(payload, jwtSignOptionsAccess),
+      this.jwtService.signAsync(payload, jwtSignOptionsRefresh),
+    ]);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
   }
 }
